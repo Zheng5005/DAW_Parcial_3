587b7f84d0d97d52c4f6766ab8de2907f113ecb9
Gungrave <jorgefranciscocz@gmail.com>
2024-06-01 23:31:37 -0600

    Frank

--- DAW_Parcial_3/Controllers/LoginController.cs
+++ DAW_Parcial_3/Controllers/LoginController.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 using System.Text.Json;
 using DAW_Parcial_3.Models;
+using System.Threading.Tasks;
 
 namespace DAW_Parcial_3.Controllers
 {
@@ -18,45 +22,101 @@ namespace DAW_Parcial_3.Controllers
             return View();
         }
 
-        public IActionResult ValidarUsuario(credenciales credenciales)
+        [HttpPost]
+        public async Task<IActionResult> ValidarUsuario(credenciales credenciales)
         {
-            //Se busca en ambas tablas para determinar a que grupo pertenecen las credenciales
-            Usuarios? usuario = (from user in _context.usuarios
-                              where user.correo == credenciales.correo
-                              && user.contrasena == credenciales.contrasena
-                              select user).FirstOrDefault();
+            var usuario = _context.usuarios
+                                  .FirstOrDefault(user => user.correo == credenciales.correo &&
+                                                          user.contrasena == credenciales.contrasena);
 
-            Empleados? empleado = (from em in _context.empleados
-                                 where em.correo == credenciales.correo
-                                 && em.contrasena == credenciales.contrasena
-                                 select em).FirstOrDefault();
+            var empleado = _context.empleados
+                                   .FirstOrDefault(em => em.correo == credenciales.correo &&
+                                                         em.contrasena == credenciales.contrasena);
 
-            //Si usuario es nulo, se asume que es un empleado
+            if (usuario != null)
+            {
+                string datosUsuario = JsonSerializer.Serialize(new
+                {
+                    usuario.id_user,
+                    usuario.correo,
+                    usuario.nombre,
+                    usuario.apellido,
+                    Tipo = "Usuario"
+                });
+
+                HttpContext.Session.SetString("usuario", datosUsuario);
+
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, usuario.correo),
+                    new Claim(ClaimTypes.Role, "Usuario"),
+                    new Claim("Id", usuario.id_user.ToString()),
+                    new Claim("Nombre", usuario.nombre),
+                    new Claim("Apellido", usuario.apellido)
+                };
+
+
+                var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                var authProperties = new AuthenticationProperties
+                {
+                    IsPersistent = true
+                };
+
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity), authProperties);
+
+                ViewBag.Mensaje = "Bienvenido";
+                return RedirectToAction("Index", "Inicio");
+            }
+
             if (empleado != null)
             {
-                String datosEmpleado = JsonSerializer.Serialize(empleado);
+                string datosEmpleado = JsonSerializer.Serialize(new
+                {
+                    empleado.id_empleado,
+                    empleado.correo,
+                    empleado.nombre,
+                    empleado.apellido,
+                    empleado.rol,
+                    Tipo = "Empleado"
+                });
                 HttpContext.Session.SetString("empleado", datosEmpleado);
+
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, empleado.correo),
+                    new Claim(ClaimTypes.Role, empleado.rol),
+                    new Claim("Id", empleado.id_empleado.ToString()),
+                    new Claim("Nombre", empleado.nombre),
+                    new Claim("Apellido", empleado.apellido)
+                };
+
+                var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                var authProperties = new AuthenticationProperties
+                {
+                    IsPersistent = true
+                };
+
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity), authProperties);
+
+                ViewBag.Mensaje = "Bienvenido";
 
                 if (empleado.rol == "Admin")
                 {
-                    ViewBag.Mensaje = "Bienvenido";
                     return RedirectToAction("IndexAdmin", "Inicio");
                 }
 
                 return RedirectToAction("IndexEmpleado", "Inicio");
             }
 
-            if (usuario != null) 
-            {
-                string datosUsuario = JsonSerializer.Serialize(usuario);
-                HttpContext.Session.SetString("usuario", datosUsuario);
-                ViewBag.Mensaje = "Bienvenido";
-
-                return RedirectToAction("Index", "Inicio");
-            }
-
-            //Se usume que es un Usuario
+            ViewBag.Mensaje = "Credenciales incorrectas!!";
             return View("Index");
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            HttpContext.Session.Clear();
+            return RedirectToAction("Index", "Login");
         }
     }
 }
--- DAW_Parcial_3/Controllers/TicketsController.cs
+++ DAW_Parcial_3/Controllers/TicketsController.cs
@@ -1,5 +1,6 @@
 ﻿using DAW_Parcial_3.Models;
 using DAW_Parcial_3.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DAW_Parcial_3.Controllers
@@ -15,9 +16,33 @@ namespace DAW_Parcial_3.Controllers
             _configuration = configuration;
         }
 
+        [Authorize]
         public IActionResult Index()
         {
+            var DatosEmpleados = (from E in _context.empleados
+                                  select new
+                                  {
+                                      id = E.id_empleado,
+                                      nombre = E.nombre,
+                                      apellido = E.apellido,
+                                      Correo = E.correo,
+                                      telefono = E.telefono,
+                                      rol = E.rol
+                                  }).ToList();
+
+            var Problemas = (from A in _context.areas 
+                             select new 
+                             { 
+                                id = A.Id_area,
+                                nombre = A.nombre
+                             });
+
+            ViewBag.Empleados = DatosEmpleados;
+            ViewBag.Problemas = Problemas;
+            
             Correo enviarCorreos = new Correo(_configuration);
+            
+            
             //Correo, Cuerpo, Asunto
             enviarCorreos.enviar("jorgefranciscocz@gmail.com", "Hola", "Prueva"); 
 
